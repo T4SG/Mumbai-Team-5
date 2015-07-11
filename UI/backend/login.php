e08fae10247f336738426596d1442239d83686b6
blessy7 <blessy.antony94@gmail.com>
2015-07-11 23:17:07 +0530

    login_1

--- UI/backend/login.php
+++ UI/backend/login.php
@@ -1,19 +1,17 @@
 <?php
-$hostname ="e.amazonaws.com";
+$hostname ="localhost";
 $database ="mydb";
 $username ="root";
 $password ="code4good";
 $localhost = @mysql_connect($hostname,$username,$password)
 or
 trigger_error(mysql_error(),E_USER_ERROR);
- 
 mysql_select_db($database, $localhost);
- 
- 
+
 $userid = $_POST['userId'];
 $password = $_POST['password'];
 
-$query_search = "select * from users where userId = '".$userid."' AND password = '".$password. "'";
+$query_search = "select * from user where userId = '".$userid."' AND password = '".$password. "'";
 $query_exec = mysql_query($query_search) or die(mysql_error());
 $rows = mysql_num_rows($query_exec);
 
