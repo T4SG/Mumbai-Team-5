1eae16688d797e2518ad607fb6d2d6b92c6e0240
Chaitali29 <chaitali2994@gmail.com>
2015-07-12 01:26:23 +0530

    9

---
+++ php/try.php
@@ -0,0 +1,14 @@
+
+<?php
+$hostname ="ec2-52-68-144-200.ap-northeast-1.compute.amazonaws.com";
+$database ="mydb";
+$username="root";
+$password ="code4good";
+echo "hello"
+$link = mysql_connect('localhost', 'root', 'code4good');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+	else
+		echo "no"
+}
+?>
