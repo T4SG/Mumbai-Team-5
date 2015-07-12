d79dd30012f196adca95ce722085e84e3c83869f
blessy7 <blessy.antony94@gmail.com>
2015-07-12 05:53:05 +0530

    18

--- UI/Calculate.php
+++ UI/Calculate.php
@@ -8,57 +8,32 @@ or
 trigger_error(mysql_error(),E_USER_ERROR);
 mysql_select_db($database, $localhost);
 
-$criteria = $_POST['criteria'];
-$pref = 1;
 $ans = array();
-$x = 1;
-if($criteria == "Budget")
-{
-	$x = 12;
-}
-else if($criteria == "Years")
-{
-	$x =8;
-}
-else if($criteria == "Impact")
-{
-	$x =3;
-}
-else if($criteria == "Availability of Teachers")
-{
-	$x =10;
-}
-$query_search = "select questionId,solution from solution group by userId";
+
+$query_search = "select * from solution";
 $query_exec = mysql_query($query_search) or die(mysql_error());
 $rows = mysql_num_rows($query_exec);
 
-
+echo $rows;
+echo "Hello";
+print_r($ans);
  if($rows == 0) { 
  echo "No Data"; 
  }
+ $b = 0;
  else  
  {
-	 $i=1;
-	 $b =1;
 	while ($row=mysql_fetch_row($query_exec))
     {
-		if($i==14)
+		for($i=4;$i<=18;$i++)
 		{
-			$i=1;
-			$b = $b+1;
+			$ans[$b] = $ans[$b]+ $row[2];
 		}
-		if($i == $x)
-		{
-			$ans[$b] = $ans[$b]+ $row[3]*2*$row[2];
-		}
-		else
-		{
-			$ans[$b] = $ans[$b]+ $row[3]*$row[2];
-		}
-		$i = $i+1;
+		$b=$b+1;
     }
 	mysql_free_result($result);
-	echo $ans;
+	print_r($ans);
+	echo "Hello Hello";
 	echo max($ans);
 }
 
